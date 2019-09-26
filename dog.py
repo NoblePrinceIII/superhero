423aebe552e633fbaccee091f5aa8813ac03ef9b
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-09-26 13:37:50 -0700

    Fixed Bugs page 2

--- dog.py
+++ dog.py
@@ -1,12 +1,28 @@
 class Dog:
-    def __init__(self, name, breed):
-        self.name = name
-        self.breed = breed
-
     def bark(self):
         print("Woof!")
 
+my_dogs = list()
+
+my_dogs.append(Dog())
+my_dogs.append(Dog())
+
+for dog in my_dogs:
+    dog.bark()
 
 
-my_dog = Dog("Rex", "Superdog")
-my_dog.bark()
+
+
+
+# class Dog:
+# #     def __init__(self, name, breed):
+# #         self.name = name
+# #         self.breed = breed
+# #
+# #     def bark(self):
+# #         print("Woof!")
+# #
+# #
+# #
+# # my_dog = Dog("Rex", "Superdog")
+# # my_dog.bark()
--- superheros.py
+++ superheros.py
@@ -8,7 +8,7 @@ class Ability:
         self.attack_strength = attack_strength
 
     def attack(self):
-        strongest_attack = random.randint(self.attack_strength, 100)
+        strongest_attack = random.randint(0, self.attack_strength)
         return strongest_attack
 
 
@@ -18,17 +18,18 @@ class Armor:
         self.name = name
         self.max_block = max_block
 
-    def block(self, max_block):
-        block = random.randitI(self.max_block, 100)
+    def block(self):
+        block = random.randint(0, self.max_block)
+        return block
 
 
 class Hero:
-    def __init__(self, name, current_health, starting_health):
+    def __init__(self, name, starting_health=100):
         self.abilities = list()
-        self.armors = list()
+        self.armor = list()
         self.name = name
         self.starting_health = starting_health
-        self.current_health = current_health
+        self.current_health = starting_health
 
     def add_ability(self, ability):
         self.abilities.append(ability)
@@ -40,19 +41,24 @@ class Hero:
         return damage_total
 
     def defend(self):
+    # adds "block" on each armor
         block_total = 0
-        for armor in self.armors:
+        for armor in self.armor:
             block_total += armor.block()
+
         return block_total
 
+
     def add_armor(self, armor):
-        self.armors.append(armor)
+    # Calculates the total damage from all ability attacks
+        self.armor.append(armor)
 
     def take_damage(self, damage):
+    # Adds armor object which is passed in self.armors
         self.current_health -= damage - self.defend()
 
     def is_alive(self):
-        if self.current_health > 0:
+        if self.current_health >= 0:
             return True
         else:
             return False
@@ -66,7 +72,8 @@ class Hero:
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Great Debugging", 50)
-    hero = Hero("Grace Hopper", 200, 200)
-    hero.add_ability(ability)
-    print(hero.abilities)
+    hero = Hero("Grace Hopper", 200)
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(15000)
+    print(hero.is_alive())
